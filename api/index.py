c1ff53e4d577cd10d553cf7764a18c90df994b67
CH Lasya <24f2005240@ds.study.iitm.ac.in>
2026-06-21 17:43:30 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,8 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["POST"],
     allow_headers=["*"],
 )
 
