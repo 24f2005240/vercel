f2e783110d696cfd19d68dce2a8489e2d9c31c49
CH Lasya <24f2005240@ds.study.iitm.ac.in>
2026-06-21 17:18:43 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["POST"],
-    allow_headers=["*"],
-)
+
+@app.get("/")
+def home():
+    return {"message": "API is working"}
 
 @app.post("/")
-async def analytics(payload: dict):
-    return {"ok": True, "received": payload}
+def predict(data: dict):
+    return {"received": data}
